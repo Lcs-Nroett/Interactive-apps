7c5540c4d1993abefa9709864ebf72f29c524699
Nathan Roett <Nroett@lcs.on.ca>
2026-02-12 10:17:50 -0500

    Initial Commit

---
+++ Interactive apps/Interactive_appsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_appsApp.swift
+//  Interactive apps
+//
+//  Created by Nathan Roett on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_appsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
